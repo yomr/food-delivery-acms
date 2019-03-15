b7b2b40f04390173c3af71ef5a6fc3c00e44c313
PrernaArora08 <prerna.smgs@gmail.com>
2019-03-15 21:39:59 +0530

    Errors Rectified

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express'),
   app = express(),
   bodyParser = require('body-parser');
 port = process.env.PORT || 3000; // For the production deployment
-
+app.listen(port);
 console.log('API server started on: ' + port);
 
 // app.use(bodyParser.urlencoded({ extended: true }));
