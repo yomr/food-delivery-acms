369bfb5676a5a94b92dde5518ed8e58b4cb0b9ee
Jhalak Naik <jhalak.naik@gmail.com>
2019-03-15 17:19:31 +0530

    Delete routes.js

--- routes.js
+++
@@ -1,8 +0,0 @@
-//const express = require('express');
-var food = require('../controller/menu');
-  
-const router = app => {
-  app.get('/getMenu/res_id=:rid', food.read_menu);
-}
-
-module.exports = router;
