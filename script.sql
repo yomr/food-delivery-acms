f01a7b97396a70999f1e8589a9499d1600767f00
JhalakNaik <jhalak.naik@gmail.com>
2019-04-04 17:51:56 +0530

    SQL common script file updated

--- script.sql
+++ script.sql
@@ -3,11 +3,11 @@ drop database food_portal;
 create database food_portal;
 use food_portal;
 
-create table Customer (user_id varchar(20) not null,firstname varchar(20) not null, lastname Varchar(20) not null, email varchar(50) not null, pwd varchar(32) not null, contact int(10) not null, address varchar(50) not null, city varchar(30) not null,Primary key(user_id));
+create table Customer (user_id varchar(20) not null,firstname varchar(20) not null, lastname Varchar(20) not null, email varchar(50) not null, pwd varchar(32) not null, contact bigint(10) not null, address varchar(50) not null, city varchar(30) not null,Primary key(user_id));
 
-create table Restaurant (user_id varchar(20) not null,name varchar(30) not null, email Varchar(50) not null, pwd varchar(32) not null, open_time time not null, close_time time not null, description varchar(30), rating int(1), gst_no varchar(32) not null, rest_type char(32) not null, address varchar(50) not null, city varchar(30) not null, Primary key(user_id));
+create table Restaurant (user_id varchar(20) not null,name varchar(30) not null, email Varchar(50) not null, pwd varchar(32) not null, open_time time not null, close_time time not null, description varchar(100), rating int(1), gst_no varchar(32) not null, rest_type char(32) not null, address varchar(50) not null, city varchar(30) not null, Primary key(user_id));
 
-create table DeliveryBoy (user_id varchar(20) not null,firstname varchar(20) not null, lastname Varchar(20) not null, email varchar(50) not null, pwd varchar(32) not null, contact int(10) not null, status varchar(20), rating int(1), vehicle_no varchar(20) not null, licence_no varchar(20) not null, Primary key(user_id));
+create table DeliveryBoy (user_id varchar(20) not null,firstname varchar(20) not null, lastname Varchar(20) not null, email varchar(50) not null, pwd varchar(32) not null, contact bigint(10) not null, status varchar(20), rating int(1), vehicle_no varchar(20) not null, licence_no varchar(20) not null, Primary key(user_id));
 
 create table Menu (res_id varchar(20) not null,item_id integer not null, itemname Varchar(20) not null, itemcategory varchar(7) not null, itemprice integer not null, Primary key(res_id,item_id), foreign key(res_id) references Restaurant(user_id));
 
